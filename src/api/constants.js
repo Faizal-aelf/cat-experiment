e72c6a0db80ddc6440ac53cee887e0ce15be18a1
Ahamed Faizal <faizal.a@aelf.io>
2024-03-14 12:21:09 +0800

    Update in the API as per loong

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,3 +1,3 @@
-export const EXPERIMENT_SUBMIT_API = "http://35.222.235.21:8000/start-task";
-export const EXPERIMENT_LIST_API = "http://34.173.215.202:7010/image/generate";
-export const UPDATE_EXPERIMENT_API = "http://34.173.215.202:7010/image/generate";
+export const EXPERIMENT_SUBMIT_API = "/experiments/start-task";
+export const EXPERIMENT_LIST_API = "/image/generate";
+export const UPDATE_EXPERIMENT_API = "/image/generate";
